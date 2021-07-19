f62be69c52a8c3080f3bd76cde2e1e5d8bad6db8
weirdodave <cidavid2018@gmail.com>
2021-07-19 09:19:06 -0500

    good!

--- davidci/src/com/huaxia/davidci/homework/HarmoneyScholl.java
+++ davidci/src/com/huaxia/davidci/homework/HarmoneyScholl.java
@@ -7,8 +7,20 @@ public class HarmoneyScholl {
 		int b = 20;
 		int c = a - b;
 		System.out.printf("Problem 01: %s - %s = %s\n",a, b, c);
+				
+		int d = 484;
+		int e = 118;
+		int f = d + e;
 		
-		System.out.println("Problem 03: I don't know how to do it.");
+		System.out.printf("Problem 02: %d + %d = %d\n",d, e, f);
+		
+		System.out.println("Problem 03: idk");
+		
+		int g = 60;
+		int h = 5;
+		int i = g / h;
+		
+		System.out.printf("problem 04: %d / %d = %d\n",g, h, i);
 	}
 
 }
